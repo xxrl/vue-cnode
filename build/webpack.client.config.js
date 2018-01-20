cbb6447cf78a9645ea629acd2a64f1a44266b6a5
lzxb <1340641314@qq.com>
2018-01-20 23:01:28 +0800

    build: 去掉无用功能

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -8,11 +8,6 @@ const config = merge(base, {
   entry: {
     app: './src/entry-client.js'
   },
-  resolve: {
-    alias: {
-      'create-api': './create-api-client.js'
-    }
-  },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
